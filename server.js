6296bfa11b281507319c8f034cd6e3e2a95668fe
Sakhawat238 <sakhawath98@gmail.com>
2019-06-21 15:10:04 +0600

    image upload: dynamic directory updated

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const multer = require('multer');
-const upload = multer({dest:'files/'});
+const path = require('path');
+
+app.use(cors());
+//app.use(express.static(__dirname + '/files/'));
+
+const storage = multer.diskStorage({
+	destination : function(req, file, cb){
+		const upload_path = path.join(__dirname, 'files');
+		cb(null, upload_path);
+	},
+	filename: function(req, file, cb){
+		cb(null, file.originalname);
+	}
+})
+const upload = multer({storage:storage});
 
 const port = process.env.PORT || 3000;
 
@@ -18,6 +33,12 @@ app.post('/img-upload',upload.single('image'), (req, res)=>{
 		'messages': 'image uploaded successfully'
 	})
 	
+})
+
+app.get('/img/:name', (req, res)=>{
+	const name = req.params.name
+	const index = path.join(__dirname, 'files', name);
+	res.sendFile(index);
 })
 
 app.listen(port, ()=>{
